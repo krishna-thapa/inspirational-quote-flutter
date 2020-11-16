300493fd9e14677038bdc04fd921e118c30a8c41
krishna <krishna.nature91@gmail.com>
2020-11-16 14:20:23 +0000

    Display quote of the day in the home page

--- lib/pages/quote/quote_of_the_day.dart
+++ lib/pages/quote/quote_of_the_day.dart
@@ -23,19 +23,31 @@ class _QuoteOfTheDayPageState extends State<QuoteOfTheDayPage> {
       builder: (context, response) {
         if (response.hasData) {
           return SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Center(
-                  child: Container(
-                    margin: EdgeInsets.only(left: 30.0, right: 10.0),
-                    child: Text(
-                      response.data.quote,
-                      style: TextStyle(fontSize: 30.0, fontFamily: 'quoteScript'),
-                    ),
+            child: Container(
+              padding: EdgeInsets.only(left: 30, right: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //Spacer(flex: 2,),
+                  Image.asset(
+                      'assets/images/quote_symbol.png',
+                    height: 60,
+                    width: 60,
+                    color: Colors.black,
                   ),
-                ),
-              ],
+                  SizedBox(height: 20,),
+                  Text(response.data.quote,
+                    style: TextStyle(fontSize: 30.0, fontFamily: 'quoteScript'),
+                  ),
+                  SizedBox(height: 30,),
+                  Text(response.data.author,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'quoteScript'),
+                  ),
+                  Text("#${response.data.genre}",
+                    style: TextStyle(fontStyle: FontStyle.italic, fontFamily: 'quoteScript'),
+                  ),
+                ],
+              ),
             ),
           );
         } else if (response.hasError) {
