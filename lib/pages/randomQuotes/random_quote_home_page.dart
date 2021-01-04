82ad3b5a48b78b0033050486245f5db5f877d3f5
krishna <krishna.nature91@gmail.com>
2021-01-04 21:26:22 +0000

    Updated action button and new quote png file

--- lib/pages/randomQuotes/random_quote_home_page.dart
+++ lib/pages/randomQuotes/random_quote_home_page.dart
@@ -10,6 +10,7 @@ class RandomQuoteHomePage extends HookWidget {
   Widget build(BuildContext context) {
     final QuoteViewModel quoteVm = useProvider(quoteProvider);
     final ThemeColor themeColor = useProvider(themeColorNotifierProvider);
+    final size = MediaQuery.of(context).size;
     return Container(
         decoration: themeColor.baseBackgroundDecoration,
         width: double.infinity,
@@ -17,108 +18,121 @@ class RandomQuoteHomePage extends HookWidget {
         child: SafeArea(
           child: quoteVm.error
               ? ErrorBody(message: quoteVm.errorMsg)
-              : Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 580.0,
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 10.0),
-                          child: Card(
-                            color: Colors.transparent,
-                            shadowColor: Colors.transparent,
-                            elevation: 4.0,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(4.0),
-                                side: BorderSide(
-                                  color: Colors.teal,
-                                  width: 5.0,
-                                )),
+              : Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 40, 10, 0),
+                      child: Image.asset(
+                        'assets/images/quote.png',
+                        width: size.width * 0.5,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 580.0,
                             child: Padding(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 20.0, vertical: 50.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Image.asset(
-                                    'assets/images/quote_symbol.png',
-                                    alignment: Alignment.centerLeft,
-                                    height: 60,
-                                    width: 60,
-                                    color: Colors.black,
-                                  ),
-                                  Text(
-                                    quoteVm.quote.quote,
-                                    style: TextStyle(
-                                        fontSize: 30.0,
-                                        fontFamily: 'quoteScript'),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    quoteVm.quote.author,
-                                    style: TextStyle(
-                                        fontSize: 20.0,
-                                        fontFamily: 'quoteScript'),
-                                  ),
-                                  Text(
-                                    quoteVm.quote.genre,
-                                    style: TextStyle(
-                                        fontSize: 20.0,
-                                        fontFamily: 'quoteScript'),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(20),
-                                    child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceAround,
-                                        children: <Widget>[
-                                          InkWell(
-                                            onTap: () {
-                                              quoteVm.getRandomQuote();
-                                            },
-                                            child: Icon(Icons.refresh,
-                                                size: 35, color: Colors.black),
-                                          ),
-                                          InkWell(
-                                            onTap: () {
-                                              // Add your onPressed code here!
-                                            },
-                                            child: Icon(Icons.content_copy,
-                                                size: 30, color: Colors.black),
-                                          ),
-                                          InkWell(
-                                            onTap: () {
-                                              // Add your onPressed code here!
-                                            },
-                                            child: Icon(Icons.share,
-                                                size: 30, color: Colors.black),
-                                          )
-                                        ]),
-                                  )
-                                ],
+                              padding: EdgeInsets.symmetric(horizontal: 10.0),
+                              child: quoteContent(quoteVm),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5.0),
+                            child: Opacity(
+                              opacity: .6,
+                              child: Text(
+                                "today's quote",
                               ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 5.0),
-                        child: Opacity(
-                          opacity: .6,
-                          child: Text(
-                            "today's quote",
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
         ));
+  }
+
+  Widget quoteContent(QuoteViewModel quoteVm) {
+    return Card(
+      color: Colors.green[300],
+      elevation: 6.0,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4.0),
+          side: BorderSide(
+            color: Colors.teal,
+            width: 5.0,
+          )),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+        child: quoteVm.loading
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/quote_symbol.png',
+                    alignment: Alignment.topLeft,
+                    height: 60,
+                    width: 60,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    quoteVm.quote.quote,
+                    style: TextStyle(fontSize: 30.0, fontFamily: 'quoteScript'),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    quoteVm.quote.author,
+                    style: TextStyle(fontSize: 20.0, fontFamily: 'quoteScript'),
+                  ),
+                  Text(
+                    quoteVm.quote.genre,
+                    style: TextStyle(fontSize: 20.0, fontFamily: 'quoteScript'),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  actionIcons(quoteVm),
+                ],
+              ),
+      ),
+    );
+  }
+
+  Widget actionIcons(QuoteViewModel quoteVm) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            InkWell(
+              onTap: () {
+                quoteVm.getRandomQuote();
+              },
+              child: Icon(Icons.refresh,
+                  size: 35, color: Colors.black),
+            ),
+            InkWell(
+              onTap: () {
+                // Add your onPressed code here!
+              },
+              child: Icon(Icons.favorite_border,
+                  size: 30, color: Colors.black),
+            ),
+            InkWell(
+              onTap: () {
+                // Add your onPressed code here!
+              },
+              child: Icon(Icons.share,
+                  size: 30, color: Colors.black),
+            )
+          ]),
+    );
   }
 }
